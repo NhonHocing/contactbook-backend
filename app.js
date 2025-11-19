4b769a593572e6cd80a5f8ff06e1ace0d159b4e3
NhonHocing <hoangnhon204@gmail.com>
2025-11-19 11:12:55 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
+const config = require("./app/config");
+const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/routes/api-error");
 
 const app = express();
 
-// Middleware (PHẢI ĐẶT TRƯỚC ROUTES)
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+  res.json({ message: "Welcome to contactbook application." });
 });
 
-// Routes - mount contact routes 
-const contactRoutes = require("./app/config/routes/contact.route");
-app.use("/api/contacts", contactRoutes);
-// Export app (PHẢI Ở CUỐI)
+app.use("/api/contacts", contactRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
